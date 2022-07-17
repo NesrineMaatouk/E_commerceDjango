4ffc6dde07cb3c0330b1b02b1957f3bbb37ea91b
NesrineMaatouk <82506911+NesrineMaatouk@users.noreply.github.com>
2022-07-17 20:32:46 +0200

    Initial commit

---
+++ category/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import category
+
+# Register your models here.
+class  categoryAdmin(admin.ModelAdmin):
+    prepopulated_fields= {'slug' : ('category_name',)}
+    list_display = ('category_name' , 'slug')
+
+admin.site.register(category,categoryAdmin)
---
+++ store/views.py
@@ -0,0 +1,38 @@
+from multiprocessing import context
+from django.shortcuts import render, get_object_or_404, render
+from .models import Product
+from category.models import category
+
+# Create your views here.
+
+def store(request,category_slug=None):
+    categories = None
+    products = None
+
+    if category_slug !=None:
+        categories = get_object_or_404(category, slug= category_slug)
+        products = Product.objects.filter(category = categories, is_available=True)
+        product_count= products.count()
+    else:
+        products = Product.objects.all().filter(is_available=True)
+        product_count= products.count()
+
+    
+
+    
+    context = {
+        'products':products,
+        'product_count':product_count,
+    }
+    return render(request, 'store/store.html',context)
+
+def product_detail(request,category_slug,product_slug):
+    try:
+        single_product = Product.objects.get(category__slug = category_slug,slug=product_slug)
+    except Exception as e:
+        raise e
+
+    context ={
+        'single_product':single_product,
+    }
+    return render (request,'store/product_detail.html',context)
